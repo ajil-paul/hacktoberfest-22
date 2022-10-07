8ca5656e59fbd541f06d78789b36ae023f2772dd
ammu-mathew <36256116+ammu-mathew@users.noreply.github.com>
2022-10-08 00:40:20 +0530

    Added Utility function to Array.js

--- utils/array.js
+++ utils/array.js
@@ -13,3 +13,13 @@ const times = (times, func) => {
   })
 }
 
+//Generate a random number within a specified range
+const getRandomNumberInRange = (lower = 0, upper = 10) => {
+  if (isNaN(lower) || isNaN(upper)) {
+    console.error("lower and upper must be valid numbers")
+    return
+  }
+  lower = Math.ceil(lower)
+  upper = Math.floor(upper)
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower
+}
